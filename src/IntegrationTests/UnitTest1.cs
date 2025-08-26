070d49e3bdaaf6ebfdb372b4e9ae517854c18421
Oleksii Tymchenko <tta19764@gmail.com>
2025-08-26 17:35:15 +0300

    Database creation and running tests in the build script.

---
+++ src/IntegrationTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace IntegrationTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // get all ChurchBulletin
+            Assert.Pass();
+        }
+    }
+}
